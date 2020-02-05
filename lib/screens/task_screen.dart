01a0cbf971c3a243539bb5c8a29856d538cb07a8
angre-kim <53202507+angre-kim@users.noreply.github.com>
2020-02-05 13:38:13 +0900

    Update task_screen.dart

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -5,15 +5,83 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Icon(Icons.list),
-          Text('TotoApp')
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                    child: Icon(
+                      Icons.list,
+                      size: 30.0,
+                      color: Colors.lightBlueAccent,
+                    )),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoapp',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '개의 할일',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+              ),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('ddd'),
+                    trailing: Checkbox(
+                      value: false,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('ddd'),
+                    trailing: Checkbox(
+                      value: false,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('ddd'),
+                    trailing: Checkbox(
+                      value: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
-
-
-
     );
   }
 }
